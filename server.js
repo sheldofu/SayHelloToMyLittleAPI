b3d877ee740a77acce0b958b684acc50a9199a1b
Sheldon Marsh <thesheldon@googlemail.com>
2015-01-06 20:10:03 +0000

    moooore

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var Post = require('./models/post')
 
 var app = express()
 app.use(bodyParser.json())
 
-app.get('/api/posts', function (req, res) {
-	res.json([
-		{
-			username: 'shelby',
-			body: 'Monsoon Moon'		
-		}
-	])
+app.get('/api/posts', function (req, res, next) {
+	Post.find(function(err,posts) {
+		if(err) { return next(err) }
+		res.json(posts)
+	})
 })
 
-var Post = require('./models/post')
 app.post('/api/posts', function (req, res, next) {
 	var post = new Post ({
 		username: req.body.username,
